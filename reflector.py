0824ec320601b1adc247ee7054637bddea2c107c
jenilshah990 <jenil@umich.edu>
2020-07-17 21:55:06 +0530

    Merge branch 'master' of https://github.com/jenilshah990/EnigmaPy # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. #Updated reflector # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- reflector.py
+++ reflector.py
@@ -5,6 +5,10 @@ Created on Fri Jul 17 21:46:47 2020
 
 @author: hetshah
 """
+Reflector_A = [4, 9, 12, 25, 0, 11, 24, 23, 21, 1, 22, 5, 2, 17, 16, 20, 14, 13, 19, 18, 15, 8, 10, 7, 6, 3]
+Reflector_B = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+Reflector_C = [5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11]
+
 def getCustomKey(s):
     """
     Parameters
@@ -28,29 +32,26 @@ class reflector(object ):
         keyset : is a string of set choosen for encryption.The default is None.
 
         """
-        ReflectorKey = []
         #if keyset not given ie Custom key case
         if keyset == None: 
-            ReflectorKey = getCustomKey(s) 
-        
+            ReflectorKey = getCustomKey(s)
         # for setA = EJMZALYXVBWFCRQUONTSPIKHGD    
-        elif keyset == 'A' or keyset == 'a' :
-            ReflectorKey = [4, 9, 12, 25, 0, 11, 24, 23, 21, 1, 22, 5, 2, 17, 16, 20, 14, 13, 19, 18, 15, 8, 10, 7, 6, 3]
-        
+        elif keyset.upper() == 'REFLECTOR_A':
+            self.wiring = Reflector_A
         # for setB = YRUHQSLDPXNGOKMIEBFZCWVJAT
-        elif keyset == 'B' or keyset == 'b':
-            ReflectorKey = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
-        
+        elif keyset.upper() == 'REFLECTOR_B':
+            self.wiring = Reflector_B
         #for setC = FVPJIAOYEDRZXWGCTKUQSBNMHL
-        elif keyset == 'C' or keyset == 'c':
-            ReflectorKey = [5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11]
+        elif keyset.upper() == 'REFLECTOR_C':
+            self.wiring = Reflector_C
+
         else:
             raise ValueError ("Error: KeySet value should be - (A,B,C,None) only")
         
         self.ReflectorKey = ReflectorKey
                 
         
-    def ReflectorOut(self,Rin):
+    def ReflectorOut(self,R_in):
         """
         Parameters
         ----------
@@ -58,5 +59,5 @@ class reflector(object ):
 
         Returns : the reflected value of Rin in ReflectorKey
         """
-        assert (Rin>=0 and Rin<26) , 'Error: Rin should between 0 and 26 only'
-        return self.ReflectorKey[Rin] 
+        assert (R_in>=0 and R_in<26) , 'Error: Rin should between 0 and 26 only'
+        return self.wiring[R_in]
